0e697266e66468734e2a5e30e7961db5c114522f
Noble Tom Mathew <nobletm@meta.com>
2025-07-03 15:32:16 -0700

    fetched player game log and extracted dates and points

--- frontend/src/components/PlayerModal.jsx
+++ frontend/src/components/PlayerModal.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState, useRef } from "react"
 import "../App.css"
+import { getPlayerGameData } from "../utils/api"
 import { Tooltip } from "./Tooltip"
 
-export function PlayerModal({onClose, data, isFav, toggleFav }) {
+export function PlayerModal({ onClose, data, isFav, toggleFav }) {
+    const [graphOption, setGraphOption] = useState("points")
+    const [playerStats, setPlayerStats] = useState([])
+    
+
+    const getPoints = async (id) => {
+        setPlayerStats((await getPlayerGameData(id)).reverse());
+    }
+
+    useEffect(() => {
+        getPoints(data.id);
+    }, [data.id])
+
+    useEffect(() => {
+        console.log(playerStats);
+    }, [playerStats])
+
     return (
         <div className="modal">
             <div className="modal-overlay" onClick={onClose}></div>
@@ -36,7 +54,7 @@ export function PlayerModal({onClose, data, isFav, toggleFav }) {
                             </div>
                     </div>
                     </div>
-                    <div className="chart"><h1>CHART GOES HERE</h1></div>
+                <canvas id="canvas"></canvas>
             </div>
         </div>
     )
