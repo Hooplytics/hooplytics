d9823be20acd6b97952a72007ab8b1b163f6bd09
Noble Tom Mathew <nobletm@meta.com>
2025-07-25 14:44:47 -0700

    added testing for sign up page

---
+++ frontend/cypress/e2e/register.cy.js
@@ -0,0 +1,21 @@
+describe('sign up page', () => {
+  it('marks required fields invalid and exposes the validationMessage', () => {
+    cy.visit('http://localhost:5173/signup');
+    cy.get('[data-cy="submit"]').click();
+
+    cy.get('input[required]').each(($input) => {
+      expect($input[0].validity.valueMissing).to.be.true;
+      expect($input[0].validationMessage).to.equal('Please fill out this field.');
+    });
+  });
+
+  // change the values whenever testing for new user
+  it("should redirect to profile page when user has inputs in all fields", () => {
+    cy.visit('http://localhost:5173/signup');
+    cy.get('[data-cy="email"]').type("test@gmail.com")
+    cy.get('[data-cy="username"]').type("test name")
+    cy.get('[data-cy="password"]').type("testpassword")
+    cy.get('[data-cy="submit"]').click();  
+    cy.url().should('match', /\/profile$/);
+  })
+});
